e308e33ec6fd21f872ef7ce91feba8e8d317f4b6
Anith Ashok <anith.ashok1910@gmail.com>
2016-09-01 12:35:32 +0530

    Created the Project Case Study and created necessary packages for it

---
+++ src/com/cdk/carbuy/controller/CarController.java
@@ -0,0 +1,8 @@
+package com.cdk.carbuy.controller;
+
+/**
+ * Created by ashoka on 9/1/2016.
+ */
+public class CarController {
+
+}
---
+++ src/com/cdk/carbuy/dao/CarDAO.java
@@ -0,0 +1,7 @@
+package com.cdk.carbuy.dao;
+
+/**
+ * Created by ashoka on 9/1/2016.
+ */
+public class CarDAO {
+}
